0d5314b0be0e29a5bd9b9c03549f9b43c845b904
NorwegianIcecube <patkar07.pk@gmail.com>
2023-05-14 19:23:24 +0200

    Finished docstrings and small restructure to improve readability

--- tm_20newsgroups.py
+++ tm_20newsgroups.py
@@ -5,13 +5,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 from tmu.models.autoencoder.autoencoder import TMAutoEncoder
 
 # Load data
-categories = ['alt.atheism', 'soc.religion.christian', 'talk.religion.misc']
-#categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware',
-              #'comp.sys.mac.hardware','comp.windows.x', 'sci.crypt', 'sci.electronics',
-              #'sci.med', 'sci.space', 'misc.forsale','talk.politics.misc',
-              #'talk.politics.guns', 'talk.politics.mideast', 'talk.religion.misc',
-              #'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey',
-              #'alt.atheism', 'soc.religion.christian']
+#categories = ['alt.atheism', 'soc.religion.christian', 'talk.religion.misc']
+categories = ['comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware',
+              'comp.sys.mac.hardware','comp.windows.x', 'sci.crypt', 'sci.electronics',
+              'sci.med', 'sci.space', 'misc.forsale','talk.politics.misc',
+              'talk.politics.guns', 'talk.politics.mideast', 'talk.religion.misc',
+              'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey',
+              'alt.atheism', 'soc.religion.christian']
 
 print("fetching training data \n")
 data_train = fetch_20newsgroups(
@@ -26,106 +26,33 @@ all_data = all_data.sample(frac=1).reset_index(drop=True)
 data_train.data = all_data.iloc[:int(len(all_data)/2)].to_numpy().flatten()
 data_test.data = all_data.iloc[int(len(all_data)/2):].to_numpy().flatten()
 
-target_words = ['jesus', 'christ', 'accept', 'trust', 'faith']
-#target_words = ['graphics', 'windows', 'ibm', 'mac', 'gun', 'cryptography',
-#                'electronics', 'medicine', 'space', 'sale', 'politics',
-#                'guns', 'mideast', 'religion', 'autos', 'motorcycles',
-#                'baseball', 'hockey', 'atheism', 'christian']
-
-
-def index_sentence(sentence, keyword):
-    words = sentence.split()
-    index = ""
-    found_index = 0
-    for i, word in enumerate(words):
-        if word == keyword:
-            found_index = i
-            break
-    if found_index == 0:
-        return sentence
-    else:
-        for i, word in enumerate(words):
-            index += f"{word}:{i - found_index} "
-    return index[:-1]
-
-def drop_post_index(sentence, indexed=True, keyword=":0"):
-    words = sentence.split()
-    sentence = ""
-    for word in words:
-        sentence += f"{word} "
-        if indexed and keyword in word:
-            break
-        elif keyword in word:
-            break
-    return sentence[:-1]
-    
-def weighted_average_precision_recall(model, num_target_words,
-                                      num_examples, vectorized_data):
-    precision = []
-    recall = []
-    f1 = []
-    all_precision, all_recall = 0, 0
-    num_p_weights,num_r_weights = 0, 0
-
-    for i in range(num_target_words):
-        clause_precision = model.clause_precision(i, True, vectorized_data,
-                                                  number_of_examples=num_examples)
-        clause_recall = model.clause_recall(i, True, vectorized_data,
-                                            number_of_examples=num_examples)
-
-        for index, clause in enumerate(zip(clause_precision, clause_recall)):
-            model_weights = model.get_weights(i)[index]
-            if not np.isnan(clause[0]):
-                weighted_precision = clause[0] * model_weights
-                all_precision += weighted_precision
-                num_p_weights += model_weights
-            if not np.isnan(clause[1]):
-                weighted_recall = clause[1] * model_weights
-                all_recall += weighted_recall
-                num_r_weights += model_weights
-        
-        try:
-            precision.append(all_precision / num_p_weights)
-        except ZeroDivisionError:
-            precision.append(0)
-        try:
-            recall.append(all_recall / num_r_weights)
-        except ZeroDivisionError:
-            recall.append(0)
-
-    for i in range(num_target_words):
-        try:
-            f1.append(2 * (precision[i] * recall[i]) / (precision[i] + recall[i]))
-        except ZeroDivisionError:
-            f1.append(0)
-
-    average_precision = np.sum(precision)/len(precision)
-    average_recall = np.sum(recall)/len(recall)
-    average_f1 = np.sum(f1)/len(f1)
-
-    return average_precision, average_recall, average_f1
+#target_words = ['jesus', 'christ', 'accept', 'trust', 'faith']
+target_words = ['graphics', 'windows', 'ibm', 'mac', 'gun', 'cryptography',
+                'electronics', 'medicine', 'space', 'sale', 'politics',
+                'guns', 'mideast', 'religion', 'autos', 'motorcycles',
+                'baseball', 'hockey', 'atheism', 'christian']
 
 def indexed_next_word_prediction_sentence(sentence, target_word):
     """ Returns the sentence with the target word indexed and the rest of the sentence is dropped"""
     if target_word not in sentence:
         return None
-    indexed_sentence = index_sentence(sentence, target_word)
+    indexed_sentence = _index_sentence(sentence, target_word)
     if indexed_sentence != sentence:
-        indexed_next_word_prediction_sentence = drop_post_index(indexed_sentence)
+        indexed_next_word_prediction_sentence = _drop_post_index(indexed_sentence)
         return indexed_next_word_prediction_sentence
 
 def indexed_missing_word_prediction_sentence(sentence, target_word):
     """ Returns the sentence with the target word indexed and masked"""
     if target_word not in sentence:
         return None
-    indexed_sentence = index_sentence(sentence, target_word)
+    indexed_sentence = _index_sentence(sentence, target_word)
     return indexed_sentence
 
 def standard_next_word_prediction_sentence(sentence, target_word):
     """ Returns the sentence with the target word dropped and the rest of the sentence is dropped"""
     if target_word not in sentence:
         return None
-    standard_next_word_prediction_sentence = drop_post_index(sentence, False, target_word)
+    standard_next_word_prediction_sentence = _drop_post_index(sentence, False, target_word)
     return standard_next_word_prediction_sentence
 
 
@@ -245,30 +172,6 @@ def create_count_vectorizer(data_train, data_test, tokenizer):
     X_test_counts = count_vect.transform(parsed_data_test)
 
     return count_vect, X_train_counts, X_test_counts
-
-def print_clauses(enc, target_words, precision, recall):
-    clauses = enc.clause_bank.number_of_clauses
-    count_vect = CountVectorizer(tokenizer=tokenizer, lowercase=False, binary=True)
-    feature_names = count_vect.get_feature_names_out()
-    print("Clauses\n")
-    for j in range(clauses):
-        print("Clause #%d " % (j), end=' ')
-        for i in range(len(target_words)):
-            print("%s: W%d:P%.2f:R%.2f " % (target_words[i], enc.get_weight(
-                i, j), precision[i][j], recall[i][j]), end=' ')
-
-        l = []
-        for k in range(enc.clause_bank.number_of_literals):
-            if enc.get_ta_action(j, k) == 1:
-                if k < enc.clause_bank.number_of_features:
-                    l.append("%s(%d)" % (
-                        feature_names[k], enc.clause_bank.get_ta_state(j, k)))
-                else:
-                    l.append("¬%s(%d)" % (
-                        feature_names[k-enc.clause_bank.number_of_features],
-                        enc.clause_bank.get_ta_state(j, k)
-                        ))
-        print(" ∧ ".join(l))
     
 def train_encoder(enc, training_data, test_data, out_file, target_words,
                   num_examples, epochs, clause_print=False):
@@ -294,9 +197,9 @@ def train_encoder(enc, training_data, test_data, out_file, target_words,
     for e in range(epochs):
         enc.fit(training_data, number_of_examples=num_examples)
 
-        model_precision, model_recall, model_f1 = weighted_average_precision_recall(enc, len(target_words),
+        model_precision, model_recall, model_f1 = _weighted_average_precision_recall(enc, len(target_words),
                                                                                     num_examples, training_data)
-        test_precision, test_recall, test_f1 = weighted_average_precision_recall(enc, len(target_words),
+        test_precision, test_recall, test_f1 = _weighted_average_precision_recall(enc, len(target_words),
                                                                                 num_examples, test_data)
         precision = []
         recall = []
@@ -324,20 +227,120 @@ def train_encoder(enc, training_data, test_data, out_file, target_words,
                                 test_precision, test_recall, test_f1]
     
     if clause_print:
-        print_clauses(enc, target_words, precision, recall)
+        _print_clauses(enc, target_words, precision, recall)
 
     return metrics_per_epoch
 
+def _index_sentence(sentence, keyword):
+    """Helper function to index the target word in the sentence."""
+    words = sentence.split()
+    index = ""
+    found_index = 0
+    for i, word in enumerate(words):
+        if word == keyword:
+            found_index = i
+            break
+    if found_index == 0:
+        return sentence
+    else:
+        for i, word in enumerate(words):
+            index += f"{word}:{i - found_index} "
+    return index[:-1]
+
+def _drop_post_index(sentence, indexed=True, keyword=":0"):
+    """Helper function to drop the post-index part of the sentence."""
+    words = sentence.split()
+    sentence = ""
+    for word in words:
+        sentence += f"{word} "
+        if indexed and keyword in word:
+            break
+        elif keyword in word:
+            break
+    return sentence[:-1]
+    
+def _weighted_average_precision_recall(model, num_target_words,
+                                      num_examples, vectorized_data):
+    """Helper function to calculate the weighted average precision and recall of the model."""
+    precision = []
+    recall = []
+    f1 = []
+    all_precision, all_recall = 0, 0
+    num_p_weights,num_r_weights = 0, 0
+
+    for i in range(num_target_words):
+        clause_precision = model.clause_precision(i, True, vectorized_data,
+                                                  number_of_examples=num_examples)
+        clause_recall = model.clause_recall(i, True, vectorized_data,
+                                            number_of_examples=num_examples)
+
+        for index, clause in enumerate(zip(clause_precision, clause_recall)):
+            model_weights = model.get_weights(i)[index]
+            if not np.isnan(clause[0]):
+                weighted_precision = clause[0] * model_weights
+                all_precision += weighted_precision
+                num_p_weights += model_weights
+            if not np.isnan(clause[1]):
+                weighted_recall = clause[1] * model_weights
+                all_recall += weighted_recall
+                num_r_weights += model_weights
+        
+        try:
+            precision.append(all_precision / num_p_weights)
+        except ZeroDivisionError:
+            precision.append(0)
+        try:
+            recall.append(all_recall / num_r_weights)
+        except ZeroDivisionError:
+            recall.append(0)
+
+    for i in range(num_target_words):
+        try:
+            f1.append(2 * (precision[i] * recall[i]) / (precision[i] + recall[i]))
+        except ZeroDivisionError:
+            f1.append(0)
+
+    average_precision = np.sum(precision)/len(precision)
+    average_recall = np.sum(recall)/len(recall)
+    average_f1 = np.sum(f1)/len(f1)
+
+    return average_precision, average_recall, average_f1
+
+
+def _print_clauses(enc, target_words, precision, recall):
+    """Helper function to print the clauses of the encoder."""
+    clauses = enc.clause_bank.number_of_clauses
+    count_vect = CountVectorizer(tokenizer=tokenizer, lowercase=False, binary=True)
+    feature_names = count_vect.get_feature_names_out()
+    print("Clauses\n")
+    for j in range(clauses):
+        print("Clause #%d " % (j), end=' ')
+        for i in range(len(target_words)):
+            print("%s: W%d:P%.2f:R%.2f " % (target_words[i], enc.get_weight(
+                i, j), precision[i][j], recall[i][j]), end=' ')
+
+        l = []
+        for k in range(enc.clause_bank.number_of_literals):
+            if enc.get_ta_action(j, k) == 1:
+                if k < enc.clause_bank.number_of_features:
+                    l.append("%s(%d)" % (
+                        feature_names[k], enc.clause_bank.get_ta_state(j, k)))
+                else:
+                    l.append("¬%s(%d)" % (
+                        feature_names[k-enc.clause_bank.number_of_features],
+                        enc.clause_bank.get_ta_state(j, k)
+                        ))
+        print(" ∧ ".join(l))
 
 if __name__ == "__main__":
     # Set Hyperparameters
-    num_examples = 100
-    clauses = 15
-    margin = 500 # How many votes needed for action
-    specificity = 2.5   # Forget value
-    accumulation = 5
-    max_literals = 20
-    epochs = 15
+    num_examples = 100      # Number of examples to train on
+    clauses = 15            # Number of clauses
+    margin = 500            # How many votes needed for action
+    specificity = 2.5       # Forget value
+    accumulation = 5        # How many other places in the dataset to get feedback from
+    max_literals = 20       # Maximum number of literals in a clause
+    epochs = 15             # Number of epochs to train for
 
     # Create dataframe to log results
     df = pd.DataFrame(columns=['epoch', 'precision', 'recall', 'f1',
@@ -358,6 +361,7 @@ if __name__ == "__main__":
         results[i] = train_encoder(enc, X_train_counts, X_test_counts, 'tm_20newsgroups.csv',
                                     target_words, num_examples, epochs, clause_print=False)
     
+    # Calculate average results
     average_results = np.zeros((epochs, 7))
     divisor_array = np.full((epochs, 7), number_of_runs)
     for run in range(len(results)):
